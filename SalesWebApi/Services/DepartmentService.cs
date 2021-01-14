e7c59c40c3c1f5ddb0de1cef25525006abdf73cf
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-14 00:22:13 -0300

    Criação de Servive\DepartmentsService.cs

---
+++ SalesWebApi/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebApi.Data;
+
+namespace SalesWebApi.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebApiContext _context;
+
+        public DepartmentService(SalesWebApiContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Department>> FindAllAsync()
+        {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
+        }
+    }
+}
