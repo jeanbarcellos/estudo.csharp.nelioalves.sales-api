b1b22d419c8c12bb57c7652f1790f7d5f1a331ff
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-14 00:24:28 -0300

    Criação de ViewModels

---
+++ SalesWebApi/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebApi.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
