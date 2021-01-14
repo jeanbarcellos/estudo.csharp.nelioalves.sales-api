6c235433a94977ca6006c47d8dcf3f9381b31af4
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-14 00:53:26 -0300

    Adição do Model SellerRecord no DbContext

--- SalesWebApi/Data/SalesWebApiContext.cs
+++ SalesWebApi/Data/SalesWebApiContext.cs
@@ -11,5 +11,7 @@ namespace SalesWebApi.Data
         public DbSet<Department> Department { get; set; }
 
         public DbSet<Seller> Seller { get; set; }
+
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
