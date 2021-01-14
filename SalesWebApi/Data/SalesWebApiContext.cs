0dd200fb91337a97ca812c37c2527102fb10fa0b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-13 23:22:15 -0300

    Criação e configuração do Data\DbContext

---
+++ SalesWebApi/Data/SalesWebApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebApi.Models;
+
+namespace SalesWebApi.Data
+{
+    public class SalesWebApiContext : DbContext
+    {
+        public SalesWebApiContext(DbContextOptions<SalesWebApiContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
--- SalesWebApi/Startup.cs
+++ SalesWebApi/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SalesWebApi.Data;
 
 namespace SalesWebApi
 {
@@ -23,9 +18,12 @@ namespace SalesWebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SalesWebApiContext>(
+              options => options.UseNpgsql(Configuration.GetConnectionString("Default"))
+            );
+            services.AddScoped<SalesWebApiContext>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -34,7 +32,6 @@ namespace SalesWebApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
