1eddee1c907fdf3b310a3238dd52d14d94557875
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-14 23:33:40 -0300

    Models - Adição de validações faltantes

--- SalesWebApi/Models/Departament.cs
+++ SalesWebApi/Models/Departament.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebApi.Models
@@ -7,6 +8,11 @@ namespace SalesWebApi.Models
     public class Department
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter no máximo 3 caracteres")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter no mínimo 60 caracteres")]
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
--- SalesWebApi/Models/Seller.cs
+++ SalesWebApi/Models/Seller.cs
@@ -9,28 +9,31 @@ namespace SalesWebApi.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O {0} é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve conter entre {2} e {1} caracteres")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "O {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [Display(Name = "Birth Date")]
+        [Display(Name = "Data de nascimento")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
-        [Display(Name = "Base Salary")]
+        [Display(Name = "Salário base")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [Range(100.0, 50000.0, ErrorMessage = "O {0} deve ser de {1} a {2}")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
+
         public int DepartmentId { get; set; }
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
