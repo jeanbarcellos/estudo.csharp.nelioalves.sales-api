937a10d63e1a154edf3dd9546cfb2e7c1ccf8c13
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-17 02:11:42 -0300

    Tentando resolver o problema da consult agrupada em SalesRecordService

--- SalesWebApi/Services/SalesRecordService.cs
+++ SalesWebApi/Services/SalesRecordService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebApi.Data;
+using SalesWebApi.Models.ViewModels;
 
 namespace SalesWebApi.Services
 {
@@ -17,7 +18,7 @@ namespace SalesWebApi.Services
             _context = context;
         }
 
-        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        public async Task<List<SimpleSearchViewModel>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
         {
             var result = from obj in _context.SalesRecord select obj;
 
@@ -33,10 +34,24 @@ namespace SalesWebApi.Services
                 .Include(x => x.Seller)
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
+                .Select(x => ItemToDto(x))
                 .ToListAsync();
         }
 
-        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        public SimpleSearchViewModel ItemToDto(SalesRecord x)
+        {
+            return new SimpleSearchViewModel
+            {
+                Id = x.Id,
+                Date = x.Date,
+                Amount = x.Amount,
+                Status = x.Status,
+                SellerName = x.Seller.Name,
+                DepartamentName = x.Seller.Department.Name
+            };
+        }
+
+        public async Task<List<GroupingSearchViewModel>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
         {
             var result = from obj in _context.SalesRecord select obj;
 
@@ -49,11 +64,21 @@ namespace SalesWebApi.Services
                 result = result.Where(x => x.Date <= maxDate.Value);
             }
 
+            DateTime minDateTime = DateTime.Parse(minDate.Value.ToString("yyyy-MM-dd"));
+            DateTime maxDateTime = DateTime.Parse(maxDate.Value.ToString("yyyy-MM-dd"));
+
+            // #TODO# Resolver problema...
             return await result
                 .Include(x => x.Seller)
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
                 .GroupBy(x => x.Seller.Department)
+                .Select(group => new GroupingSearchViewModel
+                {
+                    Department = group.Key,
+                    Items = group.ToList(),
+                    Total = group.Key.TotalSales(minDateTime, maxDateTime),
+                })
                 .ToListAsync();
         }
     }
